7441a1db20fca68019958a1a41511ae81d62284a
Skymile <mpatryk97@gmail.com>
2024-05-09 19:12:28 +0200

    Working classifier

--- KeystrokeApp/Program.cs
+++ KeystrokeApp/Program.cs
@@ -2,19 +2,29 @@
 
 const string path = @"C:\Keystrokes\";
 
-DistanceMetricCallback distance = DistanceMetrics.Euclidean;
+DistanceMetricCallback distance = DistanceMetrics.Manhattan;
 int k = 3;
 
-var all = Parser.ParseDir(path);
+var all = Parser.ParseDir(path).ToArray();
+
+int correct = 0;
 
 foreach ((int Id, Keystroke[] Keystrokes) group in all)
 {
-	Classifier.KNN(
-        new SampleSet(group.Id, group.Keystrokes)
+	int classifiedId = Classifier.KNN(
+        new SampleSet(group.Id, group.Keystrokes),
 		all .Where(i => i != group)
 			.Select(i => new SampleSet(i.Id, i.Keystrokes))
 			.ToArray(),
 		k,
 		distance
 	);
+	if (group.Id == classifiedId)
+		++correct;
+    Console.WriteLine($"{group.Id}, {classifiedId}");
 }
+
+Console.WriteLine(
+	$"Correct: {correct}/{all.Length}," +
+	$" % {Math.Round((double)correct / all.Length * 100, 2)}"
+);
