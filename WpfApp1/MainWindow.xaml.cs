a94215055553539c8b3ff4662f4bb54142e4b466
Skymile <mpatryk97@gmail.com>
2024-05-23 18:14:12 +0200

    WPF Sample

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+// MVVM
+// Model-View-ViewModel
+// Model
+//   Odpowiada za dane
+// View
+//   XAML i bindingi (wiązania danych)
+// ViewModel
+//   Dane widoku + logika
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public string SampleText 
+        { 
+            get => sampleText;
+            set 
+            {
+                sampleText = value;
+                PropertyChanged?.Invoke(this, new(nameof(SampleText)));
+            } 
+        }
+        private string sampleText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
