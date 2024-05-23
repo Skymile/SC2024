ff9ee59efcec616876190e35377ebf8c2fa2c7ff
Skymile <mpatryk97@gmail.com>
2024-05-23 18:27:35 +0200

    WPF Binding Sample

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,16 @@ using System.Windows;
 
 namespace WpfApp1
 {
+    public class MainWindowVM : ViewModel
+    {
+
+    }
+
+    public abstract class ViewModel
+    {
+
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -34,6 +44,23 @@ namespace WpfApp1
         }
         private string sampleText;
 
+        public int SampleLength
+        {
+            get => sampleLength;
+            set
+            {
+                sampleLength = value;
+                PropertyChanged?.Invoke(this, new(nameof(SampleLength)));
+            }
+        }
+        private int sampleLength;
+
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SampleText = "abc";
+            SampleLength = 4;
+        }
     }
 }
